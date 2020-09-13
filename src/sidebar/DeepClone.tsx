549b0f8f6ae648e1d7d32075ce5b3224c565df60
Jon Nutter <jon@nuorder.com>
2020-09-13 13:13:30 -0700

    add dialog and prettier

--- src/sidebar/DeepClone.tsx
+++ src/sidebar/DeepClone.tsx
@@ -27,7 +27,7 @@ type Entry = {
   },
 };
 
-export const deepClone = async (sdk: SidebarExtensionSDK, sysId: string): Promise<Entry> => {
+export const deepClone = async (sdk: SidebarExtensionSDK, sysId: string, options: { appendText: string }): Promise<Entry> => {
   const entryToClone = await sdk.space.getEntry<Entry>(sysId);
   const fields = {...entryToClone.fields};
   const keys = Object.keys(fields);
@@ -35,11 +35,11 @@ export const deepClone = async (sdk: SidebarExtensionSDK, sysId: string): Promis
     const field = fields[fieldId];
     if (field?.[DEFAULT_LOCAL]?.sys?.type === "Link") {
       if (field[DEFAULT_LOCAL].sys.linkType === "Entry" && field[DEFAULT_LOCAL].sys.id) {
-        const clonedLinkEntry = await deepClone(sdk, field[DEFAULT_LOCAL].sys.id);
+        const clonedLinkEntry = await deepClone(sdk, field[DEFAULT_LOCAL].sys.id, options);
         field[DEFAULT_LOCAL].sys.id = clonedLinkEntry.sys.id;
       }
     } else if (fieldId === "id") {
-      field[DEFAULT_LOCAL] = (field[DEFAULT_LOCAL] + " Copy") as any;
+      field[DEFAULT_LOCAL] = (field[DEFAULT_LOCAL] + " " + options.appendText) as any;
     }
   }
   return await sdk.space.createEntry(entryToClone.sys.contentType.sys.id, {
@@ -50,33 +50,39 @@ export const deepClone = async (sdk: SidebarExtensionSDK, sysId: string): Promis
 export const DeepClone: React.FC<{ sdk: SidebarExtensionSDK }> = ({ sdk }) => {
   const [isLoading, setIsLoading] = React.useState(false);
   const handleDeepClone = React.useCallback(() => {
-    const executeDeepClone = async () => {
-      try {
-        const newEntry = await deepClone(sdk, sdk.entry.getSys().id);
-        sdk.navigator.openEntry(newEntry.sys.id);
-        sdk.notifier.success("Entry deep cloned");
-      } catch {
-        sdk.notifier.error("Error cloning entry");
-      }
-      setIsLoading(false);
-    };
-    executeDeepClone();
+    sdk.dialogs
+      .openPrompt({
+        title: "Deep Clone",
+        message: "What value would like to append to cloned entry titles?",
+        defaultValue: "Copy",
+      })
+      .then(async (appendText: string | boolean) => {
+        if (appendText) {
+          try {
+            const newEntry = await deepClone(sdk, sdk.entry.getSys().id, {
+              appendText: appendText as string,
+            });
+            sdk.navigator.openEntry(newEntry.sys.id);
+            sdk.notifier.success("Entry deep cloned");
+          } catch {
+            sdk.notifier.error("Error cloning entry");
+          }
+        }
+        setIsLoading(false);
+      });
     setIsLoading(true);
 
   }, [sdk]);
   return (
     <Button
       buttonType="muted"
-      className=""
       disabled={isLoading}
-      href=""
       icon="Copy"
       indicateDropdown={false}
       isFullWidth={true}
-      // loading={isLoading}
+      loading={isLoading}
       onClick={handleDeepClone}
-      // size="large"
-      testId="cf-ui-button"
+      testId="deep-clone"
       type="button"
     >
       Deep Clone
